e16ffeaa30c9da3881721a7cde379fa071e3b04d
Sri Hari Jagan <100404902+SriHariJagan@users.noreply.github.com>
2025-09-29 18:34:50 +0530

    Add files via upload

--- src/urls.js
+++ src/urls.js
@@ -1,5 +1,9 @@
 // Base API URL
-const API_BASE = "http://localhost:5000/api";
+// export const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://maanjufarmsbackend.onrender.com/api";
+
+// ImageUrl
+export const IMAGE_URL = "http://localhost:5000/";
 
 // Auth
 export const AUTH_API = `${API_BASE}/auth`;
@@ -15,3 +19,6 @@ export const ROOMS_API = `${API_BASE}/rooms`;
 
 // Bookings
 export const BOOKINGS_API = `${API_BASE}/bookings`;
+
+// Gallery
+export const GALLERY_API = `${API_BASE}/gallery`;
