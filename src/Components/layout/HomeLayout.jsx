d72898fc08eff1e487682c5cc743cf30899bd181
Maanjoo Farms Dev <dev@maanjooFarms.com>
2026-07-16 11:39:44 +0530

    feat: add page transition animations to layout wrapper

--- src/Components/layout/HomeLayout.jsx
+++ src/Components/layout/HomeLayout.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
-import Navbar from '../common/Navbar/Navbar'
-import Footer from '../common/Footer/Footer'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useLocation } from "react-router-dom";
+import Navbar from "../common/Navbar/Navbar";
+import Footer from "../common/Footer/Footer";
+import { Outlet } from "react-router-dom";
+
+const pageTransition = {
+  initial: { opacity: 0, y: 16 },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5, ease: [0.16, 1, 0.3, 1] },
+  },
+  exit: {
+    opacity: 0,
+    y: -16,
+    transition: { duration: 0.3, ease: [0.16, 1, 0.3, 1] },
+  },
+};
 
 const HomeLayout = () => {
+  const location = useLocation();
+
   return (
     <>
       <Navbar />
-      <Outlet />
-      <Footer/>  
+      <AnimatePresence mode="wait">
+        <motion.main
+          key={location.pathname}
+          variants={pageTransition}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
+          <Outlet />
+        </motion.main>
+      </AnimatePresence>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default HomeLayout
+export default HomeLayout;
